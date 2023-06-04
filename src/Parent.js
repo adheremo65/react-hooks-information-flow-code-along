82362819f36a58088d9c472a633ed01dfaaef04d
Abdela Umer <adheremo65@gmail.com>
2023-06-03 22:28:55 -0400

    react-hooks-information-flow-code-along

--- src/Parent.js
+++ src/Parent.js
@@ -3,15 +3,23 @@ import { getRandomColor } from "./randomColorGenerator.js";
 import Child from "./Child";
 
 function Parent() {
+  function handleChangeColor() {
+    const newRandomColor = getRandomColor();
+    setColor(newRandomColor); // update color state to a new value
+  }
   const randomColor = getRandomColor();
   const [color, setColor] = useState(randomColor);
-
+  const [childrenColor, setChildrenColor] = useState("#FFF");
   return (
     <div className="parent" style={{ backgroundColor: color }}>
-      <Child />
-      <Child />
+      <Child color={childrenColor} onChangeColor={handleChangeColor} />
+      <Child color={childrenColor} onChangeColor={handleChangeColor} />
     </div>
   );
+  
+  // ...
 }
+
+
 
 export default Parent;
